4fb37d24579be1e26bed3ce95469e90b4ec7b3d6
Vo Minh Luan <minhluanc3kt@gmail.com>
2018-05-25 10:50:00 +0700

    thongbao

---
+++ thongbao.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+      <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Quản Lý Hàng Hóa</title>
+	<!-- BOOTSTRAP STYLES-->
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+     <!-- FONTAWESOME STYLES-->
+    <link href="assets/css/font-awesome.css" rel="stylesheet" />
+        <!-- CUSTOM STYLES-->
+    <link href="assets/css/custom.css" rel="stylesheet" />
+     <!-- GOOGLE FONTS-->
+   <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
+</head>
+<body>
+     
+           
+          
+    <div id="wrapper">
+         <div class="navbar navbar-inverse navbar-fixed-top">
+            <div class="adjust-nav">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="index.php">
+                    
+
+                    </a>
+                    
+                </div>
+              
+                <span class="logout-spn" >
+                  <a href="#" style="color:#fff;"</a>  
+
+                </span>
+            </div>
+        </div>
+        <!-- /. NAV TOP  -->
+        <nav class="navbar-default navbar-side" role="navigation">
+            <div class="sidebar-collapse">
+                <ul class="nav" id="main-menu">
+                 
+                    <li class="active-link">
+                        <a href="index.php" ><i class="fa fa-desktop "></i>Trang Chủ<span class="badge">Included</span></a>
+                    </li>
+                   
+
+                    <li class="active-link">
+                        <a href="products.php" ><i class="fa fa-desktop "></i>Hàng Hóa<span class="badge">Included</span></a>
+                    </li>
+
+
+                    <li>
+                        <a href="thongbao.php"><i class="fa fa-qrcode "></i>Thông Báo Hỗ Trợ</a>
+                    </li>
+                    
+
+                   
+                    
+                </ul>
+                            </div>
+
+        </nav>
+        <!-- /. NAV SIDE  -->
+        <div id="page-wrapper" >
+            <div id="page-inner">
+                <div class="row">
+                    <div class="col-lg-12">
+                     <h2>Thông Báo</h2>   
+                    </div>
+                </div>              
+                 <!-- /. ROW  -->
+                  <hr />
+                <div class="row">
+                    <div class="col-lg-12 ">
+                        <div >
+                             Để nâng cao kỹ luật, tư thế và tác phong làm việc của nhân viên phòng kế toán, nay phòng kế toán ban hành quy định khen thưởng và kỹ luật cho nhân viên thuộc bộ phận phòng kế toán .</div>
+
+<div> A/ QUY CHẾ LÀM VIỆC </div>
+
+<div>1.Đi làm đúng giờ qui đinh:</div>
+
+<div>-Sáng 7h00 đến 11h00</div>
+
+<div>-Chiều 13h00 đến 17h00</div>
+
+<div>-Làm việc từ thứ 2 đến thứ 7 ngày chủ nhật</div>
+
+<div>-Nhân viên nghỉ phải có đơn xin phép trước 02 ngày (trừ trường hợp đột xuất phải thông báo cho trưởng bộ phận). Nếu nghỉ phép 01 ngày làm đơn và nộp cho trưởng bộ phận kế toán xét duyệt và nộp nhân sự (lưu trữ), nếu nghỉ từ 02 ngày trở lên phải làm đơn nộp trưởng bộ phận xét duyệt và nộp nhân sự trình lên Ban lãnh đạo cấp trên xét duyệt</div>
+
+<div>-Nếu nhân viên xin nghỉ việc (nghỉ hẳn không còn tiếp tục công tác tại công ty) phải làm đơn trình lên giám đốc phê duyệt trước 30 ngày. Trường hợ nhân viên tự ý nghĩa việc mà chưa được sự đồng ý của ban lãnh đạo thì nhân viên đó không được hưởng bất cứ mọi chế độ, quyền lợi và bồi thường thiệt hại cho Công ty nếu gây thiệt hại nghiêm trọng nếu có</div>
+
+<div>-Chủ nhật nhân viên được thay phiên trực theo sự phân công của trưởng bộ phận, trường hợp có sự thay đổi phải báo trưởng bộ phận được biết</div>
+
+<div>2.Mặc đồng phục theo qui định công ty (nếu chưa có đồng phục thì mặc áo sơ mi trắng bỏ trong quần).</div>
+
+<div>3.Có ý thức giữ gìn vệ sinh chung sạch sẽ, ngăn nắp, giữ gìn và bảo vệ tài sản trong phòng. Hết giờ làm việc tắt điện máy tính, sắp xếp ngăn nắp gọn gàng các vật dụng trên bàn làm việc và trong phòng làm việc</div>
+
+<div>4.Không làm việc riêng, không tiếp khách trong phòng, không sử dụng điện thoại Công Ty vào việc riêng trong giờ làm việc.</div>
+
+<div>5.Khi đi ra ngoài phải có giấy ra vào cổng phải báo cáo lãnh đạo trực tiếp quản lý phòng.</div>
+
+<div>B/ QUY CHẾ KHEN THƯỞNG</div>
+
+<div>1. Hình thức khen thưởng:</div>
+
+<div>a/ Nêu gương trước toàn thể nhân viên phòng và công ty Giấy khen (Kèm theo phiếu sử dụng dịch vụ tại công ty từ 200.000 đồng đến1.000.000 đồng phiếu không có giá trị quy đổi thành tiền mặt, không trả lại tiền thừa; và được qui đổi thành các dịch vụ hiện có tại Khách Sạn.)</div>
+
+<div>b/Tăng cấp bậc, tăng lương chức vụ</div>
+
+<div>2. Đối tượng khen thưởng là toàn bộ nhân viên phòng kế toán</div>
+
+<div>3. Trường hợp khen thưởng:</div>
+
+<div>Tùy theo trường hợp cụ thể, trưởng phòng có trách nhiệm đề xuất Ban Giám Đốc ra quyết định khen thưởng cho nhân viên đạt thành tích công tác</div>
+
+<div>-Hoàn thành xuất sắc nhiệm vụ, lập công lớn trong việc nâng cao uy thế Công Ty</div>
+
+<div>-Có ý chí phấn đấu hoàn thành tốt công việc được giao, có sáng kiến, ý tưởng sáng tạo trong công việc nâng cao hiệu quả công tác kế toán cũng như sáng kiến cải thiện công tác trung phòng kế toán với các phòng ban khác trong Công Ty.</div>
+                        </div>
+                       
+                    </div>
+                    </div>
+                 
+                    </div>
+             <!-- /. PAGE INNER  -->
+            </div>
+         <!-- /. PAGE WRAPPER  -->
+        </div>
+   
